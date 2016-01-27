e5d5b387447a9b620ed173a23332cafd5c2fcc2c
Karan Pratap Singh <wizard.karan@gmail.com>
2016-01-26 23:41:27 -0500

    Changed x position of cube

--- webapp.js
+++ webapp.js
@@ -26,8 +26,7 @@ var renderer = Detector.webgl ? new THREE.WebGLRenderer() : new THREE.CanvasRend
 		var cube = new THREE.Mesh( 
 				   new THREE.BoxGeometry( 50, 50, 50 ), 
 				   new THREE.MeshBasicMaterial( {color: 0x0000ff, transparent:true} ) );
-		cube.position.y = 0.5;
-		cube.scale.x = -1;
+		cube.position.x = 0.5;
 		scene.add( cube );
 
 		var controls = new THREE.OrbitControls(camera);
